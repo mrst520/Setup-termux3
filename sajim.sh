8205e3bea2fe52ec00e7f55e8c9ec0f273111404
SAJIM Pk <68387969+Devils520@users.noreply.github.com>
2023-05-01 23:17:07 +0600

    Edited

--- sajim.sh
+++ sajim.sh
@@ -1,13 +1,8 @@
-echo 
-echo
-
+echo echo
 
 echo "Wait For All Package Download."
 
-echo
-echo
-echo
-
+echo echo echo
 
 apt update
 
@@ -17,105 +12,42 @@ termux-setup-storage
 
 pkg install python -y
 
-pkg install python2 -y
+apt uninstall python
 
-pkg install python-static -y
+apt install python
 
-pkg install python-tkinter -y
+pkg update
 
-pkg install vim-python -y
+pkg upgrade
 
-pkg install python2-static -y
+pkg install python
 
-pkg install weechat-python-plugin -y
+pip install requests
 
-pkg install git -y
+pip install mechanize
+
+pip install bs4
+
+pkg install git
+
+pip install rich
 
 pkg install figlet -y
-
-pkg install cmatrix -y
 
 pkg install toilet -y
 
 pkg install nano -y
 
-pkg install php -y
+pip install requests
 
-pkg install fish -y
-
-pip install astroid 
-
-pip install autopep8
-
-pip install certifi
-
-pip install chardet
-
-pip install colorama 
-
-pip install future
-
-pip install idna 
-
-pip install isort 
-
-pip install lazy-object-proxy
-
-pip install lolcat 
-
-pip install mccabe  
-
-pip install Pillow 
-
-pip install pilo 
-
-pip install pip
-
-pip install pycodestyle 
-
-pip install pyfiglet
-
-pip install pylint
-
-pip install requests 
-
-pip install setuptools
-
-pip install six
-
-pip install termcolor
-
-pip install toml
-
-pip install urllib3
-
-pip install wheel
-
-pip install wrapt
-
-pip install youtube-dl
-
-pip install mechanize 
-
-apt install ruby -y
-
-apt install openssh -y
+pip install mechanize
 
 apt install wget -y
 
 apt install curl -y
 
-apt install proot -y
+git clone https://github.com/Sohil876/Termux-zsh.git && cd Termux-zsh && bash setup.sh
 
-
-figlet -f slant 'SAJIM PK' |lolcat
-echo
-echo
-echo 
-figlet -f slant 'WELLCOME' |lolcat
-echo
-echo
-echo
+figlet -f slant 'SAJIM PK' |lolcat echo echo echo figlet -f slant 'WELLCOME' |lolcat echo echo echo
 
 echo "Now Your Termux Is Ready," |lolcat
-
